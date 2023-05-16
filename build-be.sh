c2da34c737558c39c5ea54c93b3e005b3ccae18d
David Yappeter <davidyap11les@gmail.com>
2023-05-16 02:19:43 +0000

    feat: initialize working deployment configuration

---
+++ build-be.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOCKER_FOLDER_PATH=/root/projects
+CAPSTONE_FOLDER_PATH=/root/projects/dicoding-capstone-backend
+
+# update local repository
+cd $CAPSTONE_FOLDER_PATH
+git stash -u && git stash clear && git pull --rebase origin "${1:-master}"
+
+# build Dockerfile and Dockerfile.tools
+cd $CAPSTONE_FOLDER_PATH
+docker build -t capstone-be -f Dockerfile .
+docker build -t capstone-be-tools -f Dockerfile.tools .
+
+# docker-compose deploy
+cd $DOCKER_FOLDER_PATH
+docker-compose up -d capstone-be
+docker-compose up -d capstone-be-tools
+
+# update docs
+docker-compose exec go make generate-docs
+
+# prune docker image
+docker image prune -f
+
