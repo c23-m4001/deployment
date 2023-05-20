39d5453e1bea808a5b863de6d5a1a5ee8a424f4c
David Yappeter <davidyap11les@gmail.com>
2023-05-20 12:33:29 +0000

    feat: add deployment for frontend

---
+++ build-fe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DOCKER_FOLDER_PATH=/root/deployment
+CAPSTONE_FOLDER_PATH=/root/deployment/frontend
+
+# update local repository
+cd $CAPSTONE_FOLDER_PATH
+git stash -u && git stash clear && git pull --rebase origin "${1:-master}"
+
+# build Dockerfile and Dockerfile.tools
+cd $CAPSTONE_FOLDER_PATH
+docker build -t capstone-fe -f Dockerfile .
+
+# docker-compose deploy
+cd $DOCKER_FOLDER_PATH
+docker-compose up -d capstone-fe
+
+# prune docker image
+docker image prune -f
+
